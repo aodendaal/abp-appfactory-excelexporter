0a6e0ac932763aecc714fa5fcbcc13c5b191d7ac
Andre Odendaal <aodendaal@gmail.com>
2018-07-05 11:29:46 +0200

    Initial commit

---
+++ ExcelExporterModule.cs
@@ -0,0 +1,17 @@
+﻿using Abp.AppFactory.BlobProvider;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using App.Factory.ExcelExport;
+
+namespace Abp.AppFactory.ExcelExport
+{
+    [DependsOn(typeof(BlobProviderModule))]
+    public class ExcelExporterModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.Register(typeof(Exporter<>));
+            IocManager.RegisterAssemblyByConvention(typeof(ExcelExporterModule).GetAssembly());
+        }
+    }
+}
